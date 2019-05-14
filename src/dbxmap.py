1012e13fc555f11059fbe08634ba6cc8a48e901b
Oscar Morata <omoratax@gmail.com>
2019-05-14 16:41:48 +0200

    [dbxmap.py] Tests done on 14.5.2019

--- src/dbxmap.py
+++ src/dbxmap.py
@@ -13,6 +13,9 @@ import matplotlib.pyplot as plt
 import aplpy
 import numpy as np
 
+from astropy import wcs
+from astropy.io import fits
+
 import yaml
 
 import os
@@ -22,15 +25,13 @@ except ImportError:
     from yaml import Loader, Dumper
 
 
-print("a", os.getcwd())
-
     
 ## defaults
 #
 config_file = 'plot_cfg.yml'
-configFilePath = (os.path.join(os.getcwd(), config_file))
-print("b", configFilePath)
-#matplotlib.use('Agg')
+#configFilePath = (os.path.join(os.getcwd(), config_file))
+#rint("b", configFilePath)
+matplotlib.use('Agg')
 
 print(" Starting...")
 
@@ -60,12 +61,39 @@ with open(config_file, 'r') as ymlfile:
 
 
 
-for section in cfg:
-    print(section)
-    print(cfg[section])
-    for sub in cfg[section]:
-        print(sub)
-        print(cfg[section][sub])
+#for section in cfg:
+#    print(section)
+#    print(cfg[section])
+#    for sub in cfg[section]:
+#        print(sub)
+#        print(cfg[section][sub])
 
 
-print(cfg['figure']['outfile'])
+#print(cfg['figure']['outfile'])
+
+
+hdulist = fits.open('test_map2.fits')
+print(hdulist.info())
+w = wcs.WCS(hdulist[0].header)
+
+center = (w.wcs.crval[0], w.wcs.crval[1])
+
+gc = []
+
+# figure definition
+#
+fig = plt.figure(figsize=(8.25, 8.25))
+
+gc.append(aplpy.FITSFigure('test_map2.fits', figure=fig, subplot=(1,1,1),
+                       dimensions=[0,1]))
+
+
+gc[0].recenter(center[0], center[1], radius=5./3600.)
+
+gc[0].show_colorscale(vmin=-7e-6, vmax=7e-5, stretch='linear', cmap='inferno',
+                      aspect='auto')
+
+gc[0].show_contour(levels=[1e-5,2e-5, 3e-5, 4e-5, 5e-5,6e-5], colors="white")
+gc[0].add_beam()
+gc[0].save('out.pdf', dpi=100)
+
